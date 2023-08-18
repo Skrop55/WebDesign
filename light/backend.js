305e7f40d055f65e023139528548b95af8f65cf4
Skrop <116880776+Skrop55@users.noreply.github.com>
2023-08-18 17:37:17 +0300

    Add files via upload

--- light/backend.js
+++ light/backend.js
@@ -1,2 +1,9 @@
-console.log("hi how are you?");
 //this is a comment
+console.log("welcome to my website!");
+
+//user input that I don't need right now
+/*
+let username = window.prompt("Type your name:");
+console.log("username =", username);
+console.log("Hi", username + ", Hope you'll enjoy the content in my website!")
+*/
